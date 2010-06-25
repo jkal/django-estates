da26535b95e86cec0c966117c81481a7a7c42c67
John Kalantzis <ikalantzis@ceid.upatras.gr>
2010-06-25 20:01:47 +0300

    UserProfile details inline in User model in Django admin.

--- places/admin.py
+++ places/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from places.models import Place, Category, UserProfile, Photo, Asset, Favorite
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin 
+
+# unregister User model to add the inline
+admin.site.unregister(User)
 
 class AssetAdmin(admin.ModelAdmin):
     pass
 
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-    
+class UserProfileInline(admin.TabularInline):
+    model = UserProfile
+
+class UserAdmin(UserAdmin):
+    inlines = [
+        UserProfileInline
+    ]
+
 class PhotoAdmin(admin.ModelAdmin):
     pass
 
@@ -18,10 +28,12 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class FavoriteAdmin(admin.ModelAdmin):
     pass
-        
+
+admin.site.register(User, UserAdmin) 
 admin.site.register(Place, PlaceAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Asset, AssetAdmin)
 admin.site.register(Favorite, FavoriteAdmin)
+# not needed since
+# admin.site.register(UserProfile, UserProfileInline)
