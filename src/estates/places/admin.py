180a11441bfed1270b574fdf76371bf739172049
jkal <ikalant@gmail.com>
2010-07-05 22:16:11 +0300

    Custom admin action to make Places published.

--- src/estates/places/admin.py
+++ src/estates/places/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from places.models import Place, Category, UserProfile, Photo, Asset, Favorite
 from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin 
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 # unregister User model to add the inline
 admin.site.unregister(User)
@@ -21,7 +22,12 @@ class PhotoAdmin(admin.ModelAdmin):
     pass
 
 class PlaceAdmin(admin.ModelAdmin):
-    pass
+    actions = ['make_published']
+    
+    def make_published(self, request, queryset):
+        queryset.update(published=True)
+    make_published.short_description = _('Mark selected places as published.')
+    
 
 class CategoryAdmin(admin.ModelAdmin):
     pass
